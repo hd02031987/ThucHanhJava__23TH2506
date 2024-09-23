33b7b7ed0b05ee44ea1be1a0b720f3542ef729f2
Nguyễn Hoàng Duy <hd02031987@gmail.com>
2024-09-23 21:35:28 +0700

    Cập nhật
    Cập nhật bài tập thực hành số 1 (tập làm quen) của môn học.

---
+++ BaiTH001_23TH2506/src/a_MyClass/MyClass.java
@@ -0,0 +1,14 @@
+package a_MyClass;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5;
+int b = 6;
+int c = a + b;
+System.out.println("a + b = " + c);
+System.out.println("Hôm nay học Java, Nhập môn còn hơi bỡ ngỡ.");
+	}
+
+}

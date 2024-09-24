9daed33bc4dbb4bad6cb5e59f66e44f1447570b7
Nguyễn Hoàng Duy <hd02031987@gmail.com>
2024-09-24 21:34:19 +0700

    Update ViDu_TinhBMI.java
    test

--- Tinh_BMI/src/ViDu_TinhBMI/ViDu_TinhBMI.java
+++ Tinh_BMI/src/ViDu_TinhBMI/ViDu_TinhBMI.java
@@ -9,29 +9,24 @@ public class ViDu_TinhBMI {
 		// TODO Auto-generated method stub
 // Tính chỉ số BMI
 // Khai báo các biến
-Scanner scanner = new Scanner(System.in);
-System.out.print("Nhập họ tên: ");
-String hoTen = scanner.nextLine();
-System.out.print("Nhập chiều cao m: ");
-double chieuCao = scanner.nextDouble();
-System.out.print("Nhập cân nặng Kg :");
-double canNang = scanner.nextDouble();
-double bmi = chieuCao/Math.pow(canNang, 2);
-if(canNang<=0) 
-	{
-	System.out.println("Chiều cao phải lớn hơn 0");
-	return;
-	}
-else if(canNang<=0)
-	{
-	System.out.println("Cân nặng phải lớn hơn 0");
-	return;
-	}
-else
-	{
-	System.out.println("BMI của " + hoTen + " là: " + bmi);
-	}
-
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Nhập họ tên: ");
+			String hoTen = scanner.nextLine();
+			System.out.print("Nhập chiều cao m: ");
+			double chieuCao = scanner.nextDouble();
+			System.out.print("Nhập cân nặng Kg :");
+			double canNang = scanner.nextDouble();
+			double bmi = canNang/Math.pow(chieuCao, 2);	
+			System.out.println("BMI của " + hoTen + " là: " + bmi);
+			String thongBao ="abc";
+			//if(bmi<16) thongBao = "Gầy mức độ III";
+			//else if(bmi<17) thongBao = "Gầy mức độ II";
+			//else if(bmi<18.5) thongBao = "Gầy mức độ I";
+			//else if(bmi<25) thongBao = "Bình thường";
+			//else if(bmi<30) thongBao = "Thừa cân";
+			//else if(bmi<35) thongBao = "Béo phì mức độ I";
+			//else if(bmi<40) thongBao = "Béo phì mức độ II";
+			//else if(bmi>16) thongBao = "Béo phì mức độ III";		
 	}
 
 }

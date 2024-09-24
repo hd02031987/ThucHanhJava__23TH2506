031ae4a3a91d3ab8225e104f8ff90b0298f979c8
Nguyễn Hoàng Duy <hd02031987@gmail.com>
2024-09-24 21:49:39 +0700

    Update ViDu_TinhBMI.java
    Cập nhật thông báo

--- Tinh_BMI/src/ViDu_TinhBMI/ViDu_TinhBMI.java
+++ Tinh_BMI/src/ViDu_TinhBMI/ViDu_TinhBMI.java
@@ -10,23 +10,33 @@ public class ViDu_TinhBMI {
 // Tính chỉ số BMI
 // Khai báo các biến
 			Scanner scanner = new Scanner(System.in);
+			String thongBao = null;
 			System.out.print("Nhập họ tên: ");
 			String hoTen = scanner.nextLine();
 			System.out.print("Nhập chiều cao m: ");
 			double chieuCao = scanner.nextDouble();
 			System.out.print("Nhập cân nặng Kg :");
 			double canNang = scanner.nextDouble();
+			//Công thức tính BMI
 			double bmi = canNang/Math.pow(chieuCao, 2);	
 			System.out.println("BMI của " + hoTen + " là: " + bmi);
-			String thongBao ="abc";
-			//if(bmi<16) thongBao = "Gầy mức độ III";
-			//else if(bmi<17) thongBao = "Gầy mức độ II";
-			//else if(bmi<18.5) thongBao = "Gầy mức độ I";
-			//else if(bmi<25) thongBao = "Bình thường";
-			//else if(bmi<30) thongBao = "Thừa cân";
-			//else if(bmi<35) thongBao = "Béo phì mức độ I";
-			//else if(bmi<40) thongBao = "Béo phì mức độ II";
-			//else if(bmi>16) thongBao = "Béo phì mức độ III";		
+			if(bmi<16) {
+				thongBao = "Gầy mức độ III";}
+			else if(bmi<17) 
+			{ thongBao = "Gầy mức độ II";}
+			else if
+			(bmi<18.5) { thongBao = "Gầy mức độ I";}
+			else if(bmi<25) 
+			{thongBao = "Bình thường";}
+			else if(bmi<30) 
+			{thongBao = "Thừa cân";}
+			else if(bmi<35) 
+			{thongBao = "Béo phì mức độ I";}
+			else if(bmi<40) 
+			{thongBao = "Béo phì mức độ II";}
+			else if(bmi>16) 
+			{thongBao = "Béo phì mức độ III";}		
+			System.out.println(thongBao);
 	}
 
 }

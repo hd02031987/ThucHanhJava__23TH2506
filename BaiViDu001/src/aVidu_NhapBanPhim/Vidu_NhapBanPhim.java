9d870b89e52d1666091044060e35a60ca6ad8ade
Nguyễn Hoàng Duy <hd02031987@gmail.com>
2024-09-24 19:08:40 +0700

    Update Vidu_NhapBanPhim.java
    Thêm dòng -------

--- BaiViDu001/src/aVidu_NhapBanPhim/Vidu_NhapBanPhim.java
+++ BaiViDu001/src/aVidu_NhapBanPhim/Vidu_NhapBanPhim.java
@@ -11,11 +11,12 @@ System.out.print("Nhập họ tên:");
 String hoTen = scanner.nextLine();
 System.out.print("Tuổi: ");
 int tuoi = scanner.nextInt();
-System.out.print("Cân nặng: ");
+System.out.print("Cân nặng (Kg): ");
 double canNang = scanner.nextDouble();
-System.out.print("Chiều cao: ");
+System.out.print("Chiều cao (cm): ");
 double chieuCao = scanner.nextDouble();
 //Xuất thông tin nhập
+System.out.println("------------------------------------------");
 System.out.println("Họ và tên: " + hoTen);
 System.out.println("Tuổi: " + tuoi);
 System.out.println("Cân nặng: " + canNang);
